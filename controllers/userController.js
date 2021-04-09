ec3cfafe1eda1d29f2f6b4a65038c35421eddc08
KeithRussel <keithmaniacop@gmail.com>
2021-04-09 18:27:36 +0800

    Add routes on userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,33 @@
-// import asyncHandler from 'express-async-handler';
 const asyncHandler = require('express-async-handler');
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const server = express();
 
 // @desc    Auth user & get token
-// @route   POST /api/users/login
+// @route   POST /login
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
   res.json('Auth User');
 });
 
+// @desc    Get user profile
+// @route   GET /profile
+// @access  Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.json('Get User Profile');
+});
+
+// @desc    Register a new user
+// @route   POST /
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.json('Register User');
+});
+
+// @desc    Get all users
+// @route   /
+// @access  Public
+const getUsers = asyncHandler(async (req, res) => {
+  res.json('Get All Users');
+});
+
 // export { authUser };
-module.exports = { authUser };
+module.exports = { authUser, getUserProfile, registerUser, getUsers };
