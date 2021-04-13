218f20c379116d5b60561f0505f46720ca4acb98
KeithRussel <keithmaniacop@gmail.com>
2021-04-13 17:35:43 +0800

    Update login page frontend style

--- pages/login.js
+++ pages/login.js
@@ -7,41 +7,40 @@ function Login() {
     <div className={styles.wrapper}>
       <div className={styles.loginBgImage}>
         <div className={styles.bgBlack}></div>
-        <Link href='#'>SociallysSzxc</Link>
+        <Link href='#'>Socially</Link>
         <h2>Get to know with others.</h2>
       </div>
-      <form>
-        <div>
-          <label for='uname'>
-            <b>Username</b>
-          </label>
-          <input
-            type='text'
-            placeholder='Enter Username'
-            name='uname'
-            required
-          />
 
-          <label for='psw'>
-            <b>Password</b>
-          </label>
-          <input
-            type='password'
-            placeholder='Enter Password'
-            name='psw'
-            required
-          />
+      <div className='m-auto'>
+        <form>
+          <h2>Login to Socially</h2>
+          <div className='d-grid'>
+            <input
+              className={styles.loginInput}
+              type='text'
+              placeholder='Enter Username'
+              name='uname'
+              required
+            />
 
-          <button type='submit'>Login</button>
-        </div>
+            <input
+              className={styles.loginInput}
+              type='password'
+              placeholder='Enter Password'
+              name='psw'
+              required
+            />
 
-        <div>
-          <button type='button'>Cancel</button>
-          <span>
-            Forgot <a href='#'>password?</a>
-          </span>
-        </div>
-      </form>
+            <input type='submit' value='Submit' />
+          </div>
+
+          <div>
+            <span>
+              Forgot <a href='#'>password?</a>
+            </span>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
