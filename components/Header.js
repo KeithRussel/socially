32ab402cefe11e6d472d9572663bed44698d8426
KeithRussel <keithmaniacop@gmail.com>
2021-04-17 15:51:46 +0800

    Header styling

--- components/Header.js
+++ components/Header.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../actions/userActions';
+import styles from '../styles/styleComponents/Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <nav className={`${styles.mainNav} container`}>
         <Link href='#'>
           <a>Socially</a>
         </Link>
-        <ul>
+        <ul className={styles.navList}>
           <li>
             <Link href='/'>
               <a>Home</a>
